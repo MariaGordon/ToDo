3dba74c626667b69a4c8e14aef8c3bde74ba69be
maria <gordon.maria@gmail.com>
2015-05-10 11:12:46 +0200

    Rename and comments

--- frontend/scripts/tasks.js
+++ frontend/scripts/tasks.js
@@ -1,16 +1,42 @@
+/*
+ * Front-end Javascript based on Facebook react.
+ * Communicates via AJAX REST API calls to back-end server
+ * 
+ * TaskBox is the main component connecting frontend to backend
+ * TaskForm enable new task creation. 
+ * TaskList component holds the complete todo-list.
+ *   Task exposes one task item
+ *    
+ * Inspired by the Facebook React tutorial building comments box.
+ */
 
-// TODO: Add checkbox and a handleOnSubmit
 var Task = React.createClass({
-	onChange: function() {
-    	this.props.done = (this.props.done === 0) ? 1 : 0;
-    	this.props.onUpdateTask(this.props.description, this.props.done)
-    },
-    render: function() {
-    
+	handleChange: function() {
+    	this.props.value["done"] = (this.props.value["done"] === 0) ? 1 : 0; 
+    	this.props.onUpdateTask(this.props.id, this.props.value["done"])
+    },    
+    render: function() {    
     return (
       <form className="task">
-      	<input type="checkbox" checked={this.props.done} onChange={this.onChange}> {this.props.description}</input>               
+      	<input type="checkbox" checked={this.props.value["done"]} onChange={this.handleChange}> {this.props.value["description"]}</input>               
       </form>        
+    );
+  }
+});
+
+var TaskList = React.createClass({
+	handleUpdate: function(id, done) {
+		this.props.onTaskUpdate(id, done);
+    },
+  render: function() {  	  			
+	var taskNodes = []
+	for (task in this.props.data) {			
+		taskNodes.push(<Task id={task} value={this.props.data[task]} onUpdateTask={this.handleUpdate}/>); 
+	};
+    return (
+      <div className="taskList">
+        {taskNodes}
+      </div>
     );
   }
 });
@@ -28,21 +54,16 @@ var TaskBox = React.createClass({
       }.bind(this)
     });
   },
-  handleNewTaskSubmit: function(newTask) {
+  handleNewTaskSubmit: function(description) {
     var tasks = this.state.data;
-    console.log(newTask)
-    tasks["task"].push(newTask);
-    console.log(tasks["task"])
+    tasks["temporary"] = {"description" : description, "done" : false};    
     this.setState({data: tasks}, function() {
-      // `setState` accepts a callback. To avoid (improbable) race condition,
-      // `we'll send the ajax request right after we optimistically set the new
-      // `state.
       $.ajax({
         url: this.props.url,
         dataType: 'json',
         contentType: 'application/json; charset=utf-8',
         type: 'POST',
-        data: JSON.stringify(newTask),
+        data: JSON.stringify({"description": description}),
         success: function(data) {
           this.setState({data: tasks});
         }.bind(this),
@@ -51,66 +72,39 @@ var TaskBox = React.createClass({
         }.bind(this)
       });
     });
-    handleUpdateTask: function(description, done) {
-        var tasks = this.state.data;
-        console.log(newTask)
-        tasks["task"].push(newTask);
-        console.log(tasks["task"])
-        this.setState({data: tasks}, function() {
-          // `setState` accepts a callback. To avoid (improbable) race condition,
-          // `we'll send the ajax request right after we optimistically set the new
-          // `state.
-          $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            contentType: 'application/json; charset=utf-8',
-            type: 'PUT',
-            data: JSON.stringify(newTask),
-            success: function(data) {
-              this.setState({data: tasks});
-            }.bind(this),
-            error: function(xhr, status, err) {
-              console.error(this.props.url, status, err.toString());
-            }.bind(this)
-          });
-        });
-
+  },
+  handleUpdateTask: function(id, done) {
+	  var tasks = this.state.data;
+	  tasks[id]["done"] = done;
+	  this.setState({data:tasks}, function() {
+		  $.ajax({
+			  url: this.props.url+"/"+id,
+			  dataType: 'json',
+			  contentType: 'application/json; charset=utf-8',
+			  type: 'PUT',
+			  data: JSON.stringify({'done': done}),
+			  success: function(data) {
+				  this.setState({data: tasks});
+			  }.bind(this),
+			  error: function(xhr, status, err) {
+				  console.error(this.props.url, status, err.toString());
+			  }.bind(this)
+		  });			
+	  	});
   },
   getInitialState: function() {
-    return {data: {"task" : []}};
+    return {data: {}};
   },
   componentDidMount: function() {
     this.loadTasksFromServer();
     setInterval(this.loadTasksFromServer, this.props.pollInterval);
   },
   render: function() {
-		console.log("yeah")
-		console.log(this.state.data)
     return (
       <div className="taskBox">
         <h1>Todos</h1>
-        <TaskList data={this.state.data} onUpdateTask={this.handleUpdateTask}/>
         <TaskForm onNewTaskSubmit={this.handleNewTaskSubmit} />
-      </div>
-    );
-  }
-});
-
-var TaskList = React.createClass({
-  render: function() {  	  		
-    var taskNodes = this.props.data["task"].map(function(task, index) {
-      return (
-        // `key` is a React-specific concept and is not mandatory for the
-        // purpose of this tutorial. if you're curious, see more here:
-        // http://facebook.github.io/react/docs/multiple-components.html#dynamic-children
-        <Task done={task.done} description={task.description} key={index} onUpdateTask={this.onUpdateTask}>
-          {task.description}
-        </Task>
-      );      
-    });
-    return (
-      <div className="taskList">
-        {taskNodes}
+        <TaskList data={this.state.data} onTaskUpdate={this.handleUpdateTask} />        
       </div>
     );
   }
@@ -123,7 +117,7 @@ var TaskForm = React.createClass({
     if (!description) {
       return;
     }
-    this.props.onNewTaskSubmit({description: description});
+    this.props.onNewTaskSubmit(description);
     React.findDOMNode(this.refs.description).value = '';    
   },
   render: function() {
